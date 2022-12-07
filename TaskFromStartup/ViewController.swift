87899e0e85a311091a37f5012f1ec20b5ae3b95e
Alik <Nigay_AO@bk.ru>
2022-12-07 18:36:13 +0600

    Initial Commit

---
+++ TaskFromStartup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskFromStartup
+//
+//  Created by Alik Nigay on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
